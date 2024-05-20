f88181b808b13020e93f2a203cd11bc648ccae14
Daphney <daphneyalexandre@hotmail.com>
2024-05-20 11:21:50 -0400

    done

---
+++ js/challenge.js
@@ -0,0 +1,12 @@
+setTimeout(code)
+setTimeout(code, delay)
+
+setTimeout(functionRef)
+setTimeout(functionRef, delay)
+setTimeout(functionRef, delay, param1)
+setTimeout(functionRef, delay, param1, param2)
+setTimeout(functionRef, delay, param1, param2, /* …, */ paramN)
+
+setTimeout(() => {
+    console.log("Delayed for 1 second.");
+  }, "1000");
